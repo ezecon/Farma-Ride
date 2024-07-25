390ad426900674ad660a7d82753f76799333d6f4
ezecon <nationhexus@gmail.com>
2024-07-25 21:04:00 +0600

    commit

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -17,29 +17,29 @@ export default function Navbar() {
           <h2 className="font-bold sm:text-lg md:text-2xl text-white">FARMA-RIDE</h2>
         </div>
         <div className="hidden md:flex lg:pr-56 lg:pt-2">
-          <ul className="flex space-x-6">
+          <ul className="flex space-x-6 pt-2">
             <li>
-              <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="#home">
+              <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="#home">
                 Home
               </a>
             </li>
             <li>
-              <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="#gallery">
+              <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="#gallery">
                 Gallery
               </a>
             </li>
             <li>
-              <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="/players">
+              <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="/players">
                 Players
               </a>
             </li>
             <li>
-              <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="/events">
+              <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="/events">
                 Events
               </a>
             </li>
             <li>
-              <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="/role">
+              <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="/role">
                 Login
               </a>
             </li>
@@ -58,19 +58,19 @@ export default function Navbar() {
       </div>
       {isMenuOpen && (
         <div className="md:hidden flex flex-col space-y-2 px-4 py-2">
-          <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="#home" onClick={toggleMenu}>
+          <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="#home" onClick={toggleMenu}>
             Home
           </a>
-          <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="#gallery" onClick={toggleMenu}>
+          <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="#gallery" onClick={toggleMenu}>
             Gallery
           </a>
-          <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="/players" onClick={toggleMenu}>
+          <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="/players" onClick={toggleMenu}>
             Players
           </a>
-          <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="/events" onClick={toggleMenu}>
+          <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="/events" onClick={toggleMenu}>
             Events
           </a>
-          <a className="text-[#ffffff] hover:underline hover:underline-offset-4" href="/role" onClick={toggleMenu}>
+          <a className="text-[#ffffff] hover:underline hover:underline-offset-4 font-mono" href="/role" onClick={toggleMenu}>
             Login
           </a>
           <a className="text-[#000000]  bg-white font-bold font-mono text-xl rounded-xl p-1 hover:bg-slate-800 hover:text-white" href="/role">
